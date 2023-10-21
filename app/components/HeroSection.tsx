55560140bc8ce3761d19b017575a6377ebc8e0a1
Kamsnyc <kamel.singh1999@gmail.com>
2023-10-21 14:48:52 -0400

    got the concept and using tailwind ui too

---
+++ app/components/HeroSection.tsx
@@ -0,0 +1,52 @@
+'use client'
+import { useState } from 'react'
+import { Dialog } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+
+const navigation = [
+  { name: 'Product', href: '#' },
+  { name: 'Features', href: '#' },
+  { name: 'Marketplace', href: '#' },
+  { name: 'Company', href: '#' },
+]
+
+export default function HeroSection() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+  return (
+        <div className="mt-10 lg:mt-20 w=full h-screen relative isolate overflow-hidden  rounded-3xl md:rounded-none md:pt-24 lg:flex lg:gap-x-10  lg:pt-0 mx-auto text-7xl ">
+    
+          <div className="mx-auto max-w-xl text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left lg:ml-16">
+            <h2 className="text-4xl font-bold tracking-tight text-black md:text-5xl">
+            Bringing Businesses and Individuals Together,
+              <br />
+              One Tap at a Time.
+            </h2>
+            <p className="mt-6 text-lg md:text-lg leading-8 text-gray-700">
+              Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla.
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
+              <a
+                href="#"
+                className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+              >
+                Get started
+              </a>
+              <a href="#" className="text-sm font-semibold leading-6 text-black">
+                Learn more <span aria-hidden="true">→</span>
+              </a>
+            </div>
+          </div>
+          <div className="relative mt-16 h-80 lg:mt-8">
+            <img
+              className="absolute rounded-md left-0 top-0 w-[57rem] max-w-none bg-white/5 ring-1 ring-white/10"
+              src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+              alt="App screenshot"
+              width={1824}
+              height={1080}
+            />
+          </div>
+        </div>
+
+  )
+}
